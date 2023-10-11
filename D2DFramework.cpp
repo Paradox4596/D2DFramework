2dbb197825844cc30d411f1ba1098533c4207eda
Yong Ha Seo <tjdydgk4596@gmail.com>
2023-10-11 18:11:55 +0900

    초기 커밋.

---
+++ D2DFramework.cpp
@@ -0,0 +1,1 @@
+#include "D2DFramework.h"
---
+++ D2DFramework.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <d2d1.h>
+#include <wrl/client.h>
+
+class D2DFramework
+{
+protected:
+	Microsoft::WRL::ComPtr<ID2D1Factory> mspD2DFactory{};
+	Microsoft::WRL::ComPtr<ID2D1HwndRenderTarget> mspRenderTarget{};
+
+
+};
+
